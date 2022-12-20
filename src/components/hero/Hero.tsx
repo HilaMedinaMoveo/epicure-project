2e369382343c5646512c7242c10c8b3e4bdba96c
Hila Medina <hilam@moveo.co.il>
2022-12-20 12:25:38 +0200

    develop

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -6,7 +6,7 @@ const Hero = () => {
 			<div className="search-and-h1">
 				<div>
 					<div className="hero-text">
-						<h1>hila msavpasv`ksbpv`dbk`</h1>
+						<h1>Epicure works with the top chef restaurants in Tel Aviv</h1>
 						<div style={{ border: "1px red solid" }}>
 							<input></input>
 						</div>
