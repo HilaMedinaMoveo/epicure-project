e585c964201ef60fc323252e3b1c410dd844a759
Hila Medina <hilam@moveo.co.il>
2022-12-20 16:53:49 +0200

    epicure-layout

--- src/components/epicurelogo/EpicureLogo.tsx
+++ src/components/epicurelogo/EpicureLogo.tsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const EpicureLogo = () => {
-	return <div className="EpicureLogo">EpicureLogo</div>;
+	return (
+		<div className="EpicureLogo">
+			<div className="epicure-logo-container">
+				<div className="epicure-logo-img">
+					<img></img>
+				</div>
+				<div className="oval-div">
+					<div></div>
+					<div></div>
+				</div>
+				<div></div>
+				<div></div>
+			</div>
+		</div>
+	);
 };
 
 export default EpicureLogo;
