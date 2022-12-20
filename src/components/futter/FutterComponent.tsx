bd2487e79f0810f5616bd35a1ce3599050cedccd
Hila Medina <hilam@moveo.co.il>
2022-12-20 16:32:32 +0200

    futter container

--- src/components/futter/FutterComponent.tsx
+++ src/components/futter/FutterComponent.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const FutterComponent = () => {
-	return <div className="FutterComponent">FutterComponent</div>;
+	return (
+		<div className="FutterComponent">
+			<div className="futter-container">
+				<span>Contact Us</span>
+				<span>Term Of Use</span>
+				<span>Privacy Policy</span>
+			</div>
+		</div>
+	);
 };
 
 export default FutterComponent;
