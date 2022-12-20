8cb69fc7a2c3139929137d73d1c7b4901c06f872
Hila Medina <hilam@moveo.co.il>
2022-12-20 15:58:21 +0200

    signature div

--- src/components/signaturedish/SignatureDish.tsx
+++ src/components/signaturedish/SignatureDish.tsx
@@ -1,7 +1,28 @@
 import React from "react";
+import spicy from "../../assets/images/spicy.svg";
+import leaf from "../../assets/images/leaf.svg";
+import oneLeaf from "../../assets/images/oneleaf.svg";
 
 const SignatureDish = () => {
-	return <div className="SignatureDish">SignatureDish</div>;
+	return (
+		<div className="SignatureDish">
+			<div className="container">
+				<p>Signature of dish</p>
+				<div>
+					<img src={spicy}></img>
+				</div>
+				<p>Spicy</p>
+				<div>
+					<img src={leaf}></img>
+				</div>
+				<p>Vegiterian</p>
+				<div>
+					<img src={oneLeaf}></img>
+				</div>
+				<p>Vegan</p>
+			</div>
+		</div>
+	);
 };
 
 export default SignatureDish;
