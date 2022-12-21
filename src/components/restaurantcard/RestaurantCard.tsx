7669748776e80a90a407eb5a19adcbc768597fa2
Hila Medina <hilam@moveo.co.il>
2022-12-21 15:31:57 +0200

    navigate

--- src/components/restaurantcard/RestaurantCard.tsx
+++ src/components/restaurantcard/RestaurantCard.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import claroPhoto from "../../assets/images/claro.svg";
+import arrowIcon from "../../assets/images/arrow.svg";
 
 const RestaurantCard = () => {
 	return (
 		<div className="card-of-resturant">
-			<div className="h1-popular">POPULAR RESTAURANT IN EPICURE</div>
 			<div className="restaurant-photo">
 				<img className="claro-photo" src={claroPhoto}></img>
 			</div>
@@ -14,7 +14,7 @@ const RestaurantCard = () => {
 			</div>
 			<div className="all-restuarant">
 				<div className="all-restuarant-link"> All Restuarant</div>
-				<img className="arrow-icon"></img>
+				<img className="arrow-icon" src={arrowIcon}></img>
 			</div>
 		</div>
 	);
--- src/components/upperHomepageCarrousel/UpperComponentCarrousel.tsx
+++ src/components/upperHomepageCarrousel/UpperComponentCarrousel.tsx
@@ -4,6 +4,7 @@ import RestaurantCard from "../restaurantcard/RestaurantCard";
 const UpperComponentCarrousel = () => {
 	return (
 		<div className="UpperComponentCarrousel">
+			<h2 className="h2-popular">POPULAR RESTAURANT IN EPICURE</h2>
 			<RestaurantCard></RestaurantCard>
 		</div>
 	);
