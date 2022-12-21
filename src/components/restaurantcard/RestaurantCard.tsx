0a5830913bd0a646a234f3fe523fd5e2fcdf2c54
Hila Medina <hilam@moveo.co.il>
2022-12-21 14:14:16 +0200

    build of card box

--- src/components/restaurantcard/RestaurantCard.tsx
+++ src/components/restaurantcard/RestaurantCard.tsx
@@ -4,12 +4,17 @@ import claroPhoto from "../../assets/images/claro.svg";
 const RestaurantCard = () => {
 	return (
 		<div className="card-of-resturant">
+			<div className="h1-popular">POPULAR RESTAURANT IN EPICURE</div>
 			<div className="restaurant-photo">
 				<img className="claro-photo" src={claroPhoto}></img>
 			</div>
 			<div className="div-of-text">
 				<div className="text1">Claro</div>
 				<div className="text2">Ran Shmueli</div>
+			</div>
+			<div className="all-restuarant">
+				<div className="all-restuarant-link"> All Restuarant</div>
+				<img className="arrow-icon"></img>
 			</div>
 		</div>
 	);
